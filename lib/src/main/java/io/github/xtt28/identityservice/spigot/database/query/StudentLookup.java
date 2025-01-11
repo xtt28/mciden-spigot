d3274bfc1e45f797eb6c3ac11283f869c781f4bc
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-01-11 07:39:55 -0500

    Add `getStudentByEmail` method

--- lib/src/main/java/io/github/xtt28/identityservice/spigot/database/query/StudentLookup.java
+++ lib/src/main/java/io/github/xtt28/identityservice/spigot/database/query/StudentLookup.java
@@ -1,5 +1,6 @@
 package io.github.xtt28.identityservice.spigot.database.query;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
@@ -15,6 +16,15 @@ public final class StudentLookup {
 
     }
 
+    private static StudentDTO createStudentDtoFromResultSet(@Nonnull final ResultSet resultSet) throws SQLException {
+        return new StudentDTO(
+                resultSet.getInt("id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name"),
+                resultSet.getString("email"),
+                resultSet.getString("academy"));
+    }
+
     public static StudentDTO getStudentByPlayerUuid(@Nonnull final MysqlDataSource dataSource, @Nonnull final UUID uuid)
             throws SQLException {
         try (final var conn = dataSource.getConnection();
@@ -23,12 +33,21 @@ public final class StudentLookup {
             stmt.setString(1, uuid.toString());
             final var resultSet = stmt.executeQuery();
             if (resultSet.next())
-                return new StudentDTO(
-                        resultSet.getInt("id"),
-                        resultSet.getString("first_name"),
-                        resultSet.getString("last_name"),
-                        resultSet.getString("email"),
-                        resultSet.getString("academy"));
+                return createStudentDtoFromResultSet(resultSet);
+
+            throw new SQLException("Record not found.");
+        }
+    }
+
+    public static StudentDTO getStudentByEmail(@Nonnull final MysqlDataSource dataSource, @Nonnull final String email)
+            throws SQLException {
+        try (final var conn = dataSource.getConnection();
+                final var stmt = conn.prepareStatement(
+                        "SELECT * FROM `students` WHERE `email` = ?;")) {
+            stmt.setString(1, email);
+            final var resultSet = stmt.executeQuery();
+            if (resultSet.next())
+                return createStudentDtoFromResultSet(resultSet);
 
             throw new SQLException("Record not found.");
         }
